5675ab28f45c1f171b11a55cbdd46e0ea07089b7
Arthur Carvalho <arthurcarvalhopsc@gmail.com>
2025-12-01 22:10:10 -0300

    Commit Atualizações

--- script-crud.js
+++ script-crud.js
@@ -4,7 +4,11 @@ const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+let tarefaSelecionada = null;
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -30,11 +34,14 @@ function criarElementoTarefa(tarefa) {
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit')
     const imagemBotao = document.createElement('img');
+    imagemBotao.classList.add('icon-edit');
+    imagemBotao.src = './imagens/edit.png';
+    botao.appendChild(imagemBotao);
 
     botao.onclick = () => {
-        debugger
+        //debugger
         const novaDescricao = prompt("Qual é o novo nome da tarefa?");
-        console.log('Nova descrição da tarefa: ', novaDescricao);
+        //console.log('Nova descrição da tarefa: ', novaDescricao);
         if (novaDescricao) {
             paragrafo.textContent = novaDescricao;
             tarefa.descricao = novaDescricao;
@@ -56,10 +63,26 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+    
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+
+        if(tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            return
+         }
+
+        tarefaSelecionada = tarefa;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
-
-
 }
 
 btnAdicionarTarefa.addEventListener('click', () => {
